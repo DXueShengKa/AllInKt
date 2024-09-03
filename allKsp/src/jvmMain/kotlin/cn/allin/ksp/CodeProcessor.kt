86a70bf9b7ef73161fe89f2035e39073b9c8f6f9
xsk <839421549@qq.com>
2024-09-04 00:37:11 +0800

    ksp生成数据库实体

--- allKsp/src/jvmMain/kotlin/cn/allin/ksp/CodeProcessor.kt
+++ allKsp/src/jvmMain/kotlin/cn/allin/ksp/CodeProcessor.kt
@@ -23,7 +23,7 @@ class CodeProcessor(environment: SymbolProcessorEnvironment) : SymbolProcessor {
 
         when {
             moduleName.contains(AllInProcessorProvider.MODULE_SERVER) -> {
-                genVo(resolver,logger)
+                generatorEntity(resolver,codeGenerator,logger)
             }
         }
 
--- allKsp/src/jvmMain/kotlin/cn/allin/ksp/ServerEntity.kt
+++ allKsp/src/jvmMain/kotlin/cn/allin/ksp/ServerEntity.kt
@@ -3,33 +3,83 @@ package cn.allin.ksp
 import com.google.devtools.ksp.KspExperimental
 import com.google.devtools.ksp.closestClassDeclaration
 import com.google.devtools.ksp.getDeclaredProperties
+import com.google.devtools.ksp.processing.CodeGenerator
+import com.google.devtools.ksp.processing.Dependencies
 import com.google.devtools.ksp.processing.KSPLogger
 import com.google.devtools.ksp.processing.Resolver
+import com.squareup.kotlinpoet.*
+import com.squareup.kotlinpoet.ParameterizedTypeName.Companion.parameterizedBy
+import com.squareup.kotlinpoet.ksp.toClassName
 import com.squareup.kotlinpoet.ksp.toTypeName
 
 @OptIn(KspExperimental::class)
-fun genVo(resolver: Resolver, logger: KSPLogger) {
+fun generatorEntity(resolver: Resolver, codeGenerator: CodeGenerator, logger: KSPLogger) {
 
     resolver.getDeclarationsFromPackage("cn.allin.exposed.table")
         .mapNotNull { ksDeclaration ->
             logger.warn(ksDeclaration.simpleName.asString())
             val classDeclaration = ksDeclaration.closestClassDeclaration()
             if (classDeclaration?.superTypes?.any {
-                it.resolve().declaration.simpleName.asString().endsWith("IdTable") } == true) {
+                    it.resolve().declaration.simpleName.asString().endsWith("IdTable")
+                } == true) {
                 classDeclaration
             } else {
                 null
             }
         }
         .forEach { d ->
+            val tableClass = d.toClassName()
+            val entityClassName = ClassName(
+                "cn.allin.exposed.entity",
+                tableClass.simpleName.run { substring(0, indexOf("Table")) } + "Entity"
+            )
+
+            val idType = d.superTypes.first().resolve().declaration.simpleName.asString().run {
+                substring(0, indexOf("IdTable"))
+            }
+            val daoPackage = "org.jetbrains.exposed.dao"
+
+            val typeSpec = TypeSpec.classBuilder(entityClassName.simpleName)
+                .superclass(ClassName(daoPackage, "${idType}Entity"))
+                .addSuperclassConstructorParameter("id")
+                .primaryConstructor(
+                    FunSpec.constructorBuilder().addParameter(
+                        "id",
+                        ClassName("$daoPackage.id", "EntityID").parameterizedBy(INT)
+                    ).build()
+                )
+                .addType(
+                    TypeSpec.companionObjectBuilder()
+                        .superclass(
+                            ClassName(daoPackage, "${idType}EntityClass")
+                                .parameterizedBy(entityClassName)
+                        )
+                        .addSuperclassConstructorParameter("%T", tableClass)
+                        .build()
+                )
+
+
             d.getDeclaredProperties()
                 .forEach {
                     val t = it.type.resolve().arguments.first().type!!.toTypeName()
-                    logger.warn("${it.simpleName.asString()} ")
+                    val propertyName = it.simpleName.asString()
+                    val propertySpec = PropertySpec.builder(
+                        propertyName,
+                        t
+                    ).delegate("%T.$propertyName", tableClass)
+                    typeSpec.addProperty(propertySpec.build())
                 }
-            logger.warn(d.simpleName.asString())
+
+           val file = FileSpec.builder(entityClassName)
+                .addType(typeSpec.build())
+                .build()
+
+            codeGenerator.createNewFile(
+                Dependencies(true, d.containingFile!!),
+                entityClassName.packageName,
+                entityClassName.simpleName,
+            ).writer().use {
+                file.writeTo(it)
+            }
         }
-
-    error("")
-
 }
