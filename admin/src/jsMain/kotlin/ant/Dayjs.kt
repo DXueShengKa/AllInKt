a0b98b87e0c4e3dce0b02fc0e3432396d538a10c
xsk <839421549@qq.com>
2025-01-11 11:57:15 +0800

    dayjs 声明 0.1

--- admin/src/jsMain/kotlin/ant/Dayjs.kt
+++ admin/src/jsMain/kotlin/ant/Dayjs.kt
@@ -1,11 +1,36 @@
-@file:JsModule("dayjs")
+//@file:JsModule("dayjs")
 
 package ant
 
+import kotlin.js.Date
 
-external class Dayjs {
-//    fun locale(locale: String): Dayjs
-    fun locale(): String
+@JsModule("dayjs")
+@JsNonModule
+external object dayjs {
+    fun apply(date: Date? = definedExternally): Dayjs
+    fun apply(date: String? = definedExternally): Dayjs
+    fun apply(date: Number? = definedExternally): Dayjs
+    fun apply(date: Dayjs? = definedExternally): Dayjs
+    fun apply(): Dayjs
+
+    fun unix(timestamp: Number): Dayjs
+    fun isDayjs(obj: Any?): Boolean
 }
 
-external val dayjs: Dayjs
+external interface Dayjs {
+    fun format(pattern: String = definedExternally): String
+    fun add(value: Number, unit: String): Dayjs
+    fun subtract(value: Number, unit: String): Dayjs
+    fun startOf(unit: String): Dayjs
+    fun endOf(unit: String): Dayjs
+    fun diff(date: Dayjs, unit: String = definedExternally): Number
+    fun toDate(): Date
+    fun toJSON(): String
+    fun isBefore(date: Dayjs): Boolean
+    fun isAfter(date: Dayjs): Boolean
+    fun isSame(date: Dayjs): Boolean
+    fun isValid(): Boolean
+}
+
+// 扩展函数，方便使用
+fun Dayjs.formatDefault(): String = format("YYYY-MM-DD HH:mm:ss")
