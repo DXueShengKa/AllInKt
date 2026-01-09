63d4ddfe9a5ddaf8e215876d1a4e29521c41ddbd
xsk <839421549@qq.com>
2026-01-09 18:05:36 +0800

    添加灵码规则

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -10,7 +10,6 @@ plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.compose)
-//    alias(libs.plugins.jetbrains.compose.hotReload)
     alias(libs.plugins.kotlin.composeCompiler)
     alias(libs.plugins.ksp)
 }
